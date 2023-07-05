fe0b0687605e6a217d07c56c5cf952faec205cc6
Hildegunn80 <94168921+Hildegunn80@users.noreply.github.com>
2023-07-05 06:29:59 +0200

    adjusting number of posts
    adjusting number of posts to 14

--- js/index.js
+++ js/index.js
@@ -1,14 +1,14 @@
 
 
-const url = "http://10.20.21.208/Lowcarbheaven/wordpress/wp-json/wp/v2/posts?_embed";
+const url = "http://10.20.21.208/Lowcarbheaven/wordpress/wp-json/wp/v2/posts?_embed&per_page=14";
 const container = document.querySelector(".latest-posts");
+let json;
 
 async function fetchPosts() {
     try {
         const response =await fetch(url);
         const result =await response.json();
-        const post = result;
-        createHTML(post);
+        createHTML(result);
     }
     catch (error){
         console.log(error);
@@ -31,8 +31,10 @@ function createHTML(post) {
             let source_url = featuredmedia['0'].source_url;
 
             container.innerHTML+= ` <section class="carousel">
+                                    <div class"content-card-1">
                                     <div class="article"><h2>${post.title.rendered}</h2>
                                     <img src="${source_url}"></div>
+                                    </div>
                                     </section>
                                     `;
 
