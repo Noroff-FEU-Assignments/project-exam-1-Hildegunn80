06427d2eb6681e87b909bfa428775e3bf7f8c605
Hildegunn80 <94168921+Hildegunn80@users.noreply.github.com>
2023-07-13 14:45:09 +0200

    Adding js to bloglist
    Adding js to bloglist

--- js/bloglist.js
+++ js/bloglist.js
@@ -1,2 +1,69 @@
-const url = "http://10.20.21.208/Lowcarbheaven/wordpress/wp-json/wp/v2/posts?_embed&per_page=100";
-const container = document.querySelector(".latest-posts");
+
+const blogContainer = document.querySelector(".blogcontainer");
+const morePosts = document.getElementsByClassName(".morePosts");
+let json;
+let index = 0;
+
+fetchJson().then(
+    function (result) {
+        json = result;
+        createHTML();
+    }
+)
+
+function configureButton() {
+    console.log("Configure button")
+    morePosts.addEventListener("click", viewMore);
+}
+
+
+function viewMore() {
+    index+=9;
+    createHTML(index);
+}
+
+function createHTML() {
+
+    // boundary check
+    if(index < 0) {
+        index = 0;
+    }
+    let end = index + 9
+    if(end > json.length) {
+        end = json.length;
+        index = json.length -9;
+    }
+
+    blogContainer.innerHTML = "";
+    for (let i = index; i < end; i++) {
+        //console.log("Title: " + json[i].title.rendered);
+
+        let featuredmedia = json[i]._embedded['wp:featuredmedia'];
+
+        if (typeof featuredmedia == "undefined") {
+            console.log("missing featuredmedia, skipping");
+            continue;
+        }
+
+        let source_url = featuredmedia['0'].source_url;
+        source_url = source_url.replace("localhost","10.20.21.208");    // workaround
+        //console.log ("url: "+source_url);
+
+        blogContainer.innerHTML += ` <section class="carousel">
+                                <div class"content-card-1">
+                                <div class="article"><h2>${json[i].title.rendered}</h2>
+                                <img src="${source_url}"></div>
+                                </div>
+                                </section>
+                                `;
+    }    
+    configureButton();
+
+    fetchJson().then(
+        function (result) {
+            json = result;
+            createHTML();
+        }
+        )
+
+}
