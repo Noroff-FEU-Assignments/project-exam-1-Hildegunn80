700766d32148ef93ee01a71a6389bd8e953daffd
Hildegunn80 <hildegunn.bjelland@gmail.com>
2023-09-10 21:40:09 +0200

    api host update

--- js/common.js
+++ js/common.js
@@ -1,4 +1,4 @@
-const url = "http://lowcarb-api.not.nu/wp-json/wp/v2/posts?_embed&per_page=100";
+const url = "http://lowcarb.not.nu/backend/wp-json/wp/v2/posts?_embed&per_page=100";
 
 async function fetchJson() {
     try {
