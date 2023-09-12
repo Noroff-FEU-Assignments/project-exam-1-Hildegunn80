cfec26d994b5fa1ef6f701ee5125b098b090d4fe
Hildegunn80 <94168921+Hildegunn80@users.noreply.github.com>
2023-09-12 19:55:29 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -67,7 +67,7 @@ function createHTML() {
                                     <div class"content-card-1">
                                         <div class="article"><h2>${json[i].title.rendered}</h2>
                                           <img src="${source_url}"></div>
-                                          <a href ="blogpost.html?id=${json[i].id}"><button class="readmore" title="click to read this post">Read post</a></button>
+                                          <a href ="blogpost.html?id=${json[i].id}"><button class="readmore" title="click to read this post"><i class="fa-solid fa-utensils" title="knive and fork"></i></a>Read more</button>
                                        </div>
                                     </div>
                                 </section>
