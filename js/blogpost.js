1e10c2d6b515c6aae92229f500eda7c498065dab
Hildegunn80 <94168921+Hildegunn80@users.noreply.github.com>
2023-09-16 12:59:52 +0200

    Update blogpost.js
    adding js for comment form validation

--- js/blogpost.js
+++ js/blogpost.js
@@ -1,3 +1,15 @@
+const form = document.querySelector("#commentForm");
+
+const comment = document.querySelector ("#messagebox");
+const messageError = document.querySelector("#messageError");
+const firstName = document.querySelector("#FirstName");
+const firstnameError =document.querySelector("#FnameError");
+const lastName = document.querySelector("#LastName");
+const lastNameError =document.querySelector("#LnameError");
+const e_mail = document.querySelector("#email");
+const emailError = document.querySelector("#emailError");
+const formMessageValidated = document.querySelector("#form_validated");
+const buttonCloseMessage = document.querySelector(".close-message");
 const blogpostContainer = document.querySelector(".blogpostcontainer");
 const banner = document.querySelector(".banner");
 const queryString =document.location.search;
@@ -82,4 +94,73 @@ function enableModal() {
 
     modal.addEventListener('click', modalClick);
 }
+
+
+//* Validate comment form
+
+function validateForm() {
+    try {
+        event.preventDefault();
+
+        let success = true;
+        if(isLenghtValid(firstName.value,5)===true) {
+            firstnameError.style.display ="none"
+        } else {
+            success = false;
+            firstnameError.style.display ="block"
+        }
+
+        if(isLenghtValid(lastName.value,5)===true) {
+            lastNameError.style.display ="none"
+        } else {
+            success = false;
+            lastNameError.style.display ="block"
+        }
+
+
+        if(isLenghtValid(comment.value,10)===true) {
+            messageError.style.display ="none"
+        } else {
+            success = false;
+            messageError.style.display ="block"
+        }
+
+        if(isEmailValid(email.value)===true) {
+            emailError.style.display ="none"
+        } else {
+            success = false;
+            emailError.style.display ="block"
+        }
+
+        if(success) {
+            formMessageValidated.style.display=("block")
+            form.reset();
+        }
+    }
+    catch(error) {               
+        createHtmlError(error,".blogpostcontainer");
+    }
+}
+
+form.addEventListener("submit", validateForm );
+buttonCloseMessage.addEventListener("click", closeMessage);
+
+function closeMessage() {
+    formMessageValidated.style.display=("none")
+} 
+
+function isEmailValid(email) {
+    const regEx = /\S+@\S+\.\S+/;
+    const patternMatches = regEx.test(email);
+    return patternMatches;
+}
+
+function isLenghtValid(value,len ) {
+    if (value.trim().length>=len) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
 
